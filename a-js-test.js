313b7bffdc39c6f209271de968b2e90b3caff8b9
Zaid Moreno <zaidmoreno@gmail.com>
2019-05-26 13:08:01 -0700

    JavaScript sandbox

--- a-js-test.js
+++ a-js-test.js
@@ -1,3 +1,14 @@
-var courses = new Array("HTML", "CSS", "JavaScript");
+// var courses = new Array('HTML', 'CSS', 'JavaScript');
 
-alert("Hello!");
+// alert(Math.PI);
+
+var elem = document.getElementById('demo');
+elem.innerHTML = 'It worked!';
+
+var arr = document.getElementsByTagName('p');
+for (var x = 0; x < arr.length; x++) {
+	arr[x].innerHTML = 'For loop firing!';
+}
+
+var el = document.getElementById('myimg');
+el.src = 'images/logo.png';
